75028680d16ce813d365d7ff5287ac4fd0626a06
Souwg <sansanvzla@gmail.com>
2024-12-28 22:31:21 +0000

    cambios en la base de datos

--- src/api/models.py
+++ src/api/models.py
@@ -4,16 +4,21 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), default=True)
+    is_admin = db.Column(db.Boolean(), default=False)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<User {self.email, self.is_admin,}>'
 
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
+            "last_name": self.last_name,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "is_admin": self.is_admin,
         }
