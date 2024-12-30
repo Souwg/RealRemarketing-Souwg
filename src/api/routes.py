51934e6ada5574ad62b87563302d516c93e84d3d
Souwg <sansanvzla@gmail.com>
2024-12-30 03:37:19 +0000

    login view

--- src/api/routes.py
+++ src/api/routes.py
@@ -22,3 +22,24 @@ def handle_hello():
     return jsonify(response_body), 200
 
 #register endpoint
+@api.route('/signup', methods=['POST'])
+def signup_user():
+    body=request.get_json()
+    if body['name'] is None:
+        return jsonify({"msg":"This field is required."})
+    if body['email'] is None:
+        return jsonify({"msg":"the email is required"}),400
+    if body['password'] is None:
+        return jsonify({"msg":"the password is required"}),400
+    
+    existing_user = User.query.filter_by(email=body['email']).first()
+    if existing_user:
+        return jsonify({"msg":"This email is already registered."}), 400
+    
+    #The password is hashed and encrypted for security
+    body['password']=bcrypt.generate_password_hash(body['password']).decode('utf-8')
+    #It is stored in the database
+    user = User(name=body['name'], last_name=body['last_name'], email=body['email'], password=body['password'], is_active=True)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"msg": "User created successfully", "user":user.serialize()})
